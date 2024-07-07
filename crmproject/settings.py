269fc7b09eca154a444bf25d6ea7e7d806a7626b
plus Ultra <117388186+Ladla18@users.noreply.github.com>
2024-07-08 00:41:16 +0530

    Update settings.py

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz=x4x2vamibl(w-6w=ovbtd7z9$0-7d+^4ezc@ism=e!^qv%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
