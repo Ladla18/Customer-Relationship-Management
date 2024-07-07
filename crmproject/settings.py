a7edbdadaaffffb458590da6ff51ea7222879a8c
plus Ultra <117388186+Ladla18@users.noreply.github.com>
2024-07-07 21:42:03 +0530

    DEBUG to false

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rz=x4x2vamibl(w-6w=ovbtd7z9$0-7d+^4ezc@ism=e!^qv%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
